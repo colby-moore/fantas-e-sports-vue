816af61211eaa46076a22ec0496f7220536454ac
Brendan Hegarty <bthegarty@gmail.com>
2019-08-26 21:28:33 -0400

    add vuex/router to main js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Vuex from 'vuex'
 import App from './App.vue'
 import BootstrapVue from 'bootstrap-vue'
 
@@ -7,6 +9,8 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue)
+Vue.use(VueRouter)
+Vue.use(Vuex)
 
 Vue.config.productionTip = false
 
