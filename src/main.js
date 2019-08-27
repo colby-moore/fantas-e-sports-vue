6985c354e778b78884ba6c65a35e1474e7937201
Brendan Hegarty <bthegarty@gmail.com>
2019-08-26 22:31:01 -0400

    inital router setup

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
 import Vuex from 'vuex'
 import App from './App.vue'
 import BootstrapVue from 'bootstrap-vue'
+import router from './router'
 
 //bootstrap css
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue)
-Vue.use(VueRouter)
 Vue.use(Vuex)
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
