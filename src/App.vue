ec2dcac47c1d8983e169b4b17ee7c79eaf923b20
colby-moore <colbym2014@gmail.com>
2019-12-16 21:37:32 -0500

    Cleaned up merge errors

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,15 @@
 <template>
   <div id="app">
-    <!-- <navbar />
-    <landing /> -->
-    <!-- <login /> -->
     <router-view/>
   </div>
 </template>
 
 <script>
-import navbar from './components/navbar.vue'
-import landing from './components/landing.vue'
-import login from './components/login.vue'
 
 export default {
   name: 'app',
   components: {
-    navbar,
-    landing,
-    login
+
   },
   created(){
   
